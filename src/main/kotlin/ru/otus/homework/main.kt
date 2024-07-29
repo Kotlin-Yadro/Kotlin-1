e285ca3f28c39dac7a6fafb22dd5a48f8ab315a2
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-07-29 11:13:16 +0300

    for indexed

--- src/main/kotlin/ru/otus/homework/main.kt
+++ src/main/kotlin/ru/otus/homework/main.kt
@@ -2,8 +2,8 @@ package ru.otus.homework
 
 fun main() {
     val ints = arrayOf(1, 2, 3, 4, 5)
-    for (i: Int in ints) { // Перебор массива
-        println(i)         // Вывод элемента массива
+    for ((i, v) in ints.withIndex()) { // Перебор массива с индексами
+        println("index: $i, value: $v")                     // index: 0, value: 1
     }
 }
 
