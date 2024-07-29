6bd60cbab12fce4fe93ceed9ea8b18f9ddca435a
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-07-29 09:39:34 +0300

    Import

--- src/main/kotlin/ru/otus/homework/main.kt
+++ src/main/kotlin/ru/otus/homework/main.kt
@@ -1,6 +1,8 @@
 package ru.otus.homework
 
+import ru.otus.homework.vasya.MyClass
+
 fun main() {
-    println("From Vasya: ${ru.otus.homework.vasya.MyClass()}")
+    println("From Vasya: ${MyClass()}")
     println("From Petya: ${ru.otus.homework.petya.MyClass()}")
 }
