c2a6e9c4ebd936602b51e629acef53acb895c246
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-07-29 10:43:27 +0300

    L, R values

--- src/main/kotlin/ru/otus/homework/main.kt
+++ src/main/kotlin/ru/otus/homework/main.kt
@@ -2,7 +2,12 @@ package ru.otus.homework
 
 fun main() {
     var a = 1
-    a = 2
+    a = 2 + 3
+    a = someInt()
+}
+
+fun someInt(): Int {
+    return 100500
 }
 
 fun joinStrings(a: String, b: String): String {
