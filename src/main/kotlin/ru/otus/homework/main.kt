fa0fd8f859479c39cf433d2002e174a19f312358
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-07-29 10:41:51 +0300

    Var

--- src/main/kotlin/ru/otus/homework/main.kt
+++ src/main/kotlin/ru/otus/homework/main.kt
@@ -1,11 +1,8 @@
 package ru.otus.homework
 
 fun main() {
-    val a1 = arrayOf(1, 2, 3)
-    val a2 = arrayOf(4, 5)
-    val a3 = a1 + a2 // a3 - новый массив!
-    val a4 = a3 + 6  // a4 - новый массив!
-    println("Array contents: ${a4.joinToString()}")
+    var a = 1
+    a = 2
 }
 
 fun joinStrings(a: String, b: String): String {
