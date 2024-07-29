c15d9776f5f21148025ef10918eb329e4942eda6
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-07-29 10:49:18 +0300

    when case expression

--- src/main/kotlin/ru/otus/homework/main.kt
+++ src/main/kotlin/ru/otus/homework/main.kt
@@ -5,7 +5,7 @@ import kotlin.random.Random
 fun main() {
     val a: String = when(Random.nextInt(0, 5)) {
         0 -> "Hello"
-        1, 2, 3 -> "World"
+        in 3 .. 4 -> "World"
         else -> "!"
     }
     println(a)
