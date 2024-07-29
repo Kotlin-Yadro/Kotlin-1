9a54e0a0758d7533b5d3575c19eb7194d475d106
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-07-29 11:15:12 +0300

    for basic

--- src/main/kotlin/ru/otus/homework/main.kt
+++ src/main/kotlin/ru/otus/homework/main.kt
@@ -2,8 +2,8 @@ package ru.otus.homework
 
 fun main() {
     val ints = arrayOf(1, 2, 3, 4, 5)
-    for (i in ints.indices) { // Перебор массива с индексами
-        println("index: $i, value: ${ints[i]}") // index: 0, value: 1
+    for (i in 0 until ints.size) {
+        println("index: $i, value: ${ints[i]}")
     }
 }
 
