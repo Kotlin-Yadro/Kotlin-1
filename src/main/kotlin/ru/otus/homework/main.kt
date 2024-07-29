a0d05fa809b7e6feedfd43385d947a195692a478
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-07-29 10:26:34 +0300

    Vals

--- src/main/kotlin/ru/otus/homework/main.kt
+++ src/main/kotlin/ru/otus/homework/main.kt
@@ -1,8 +1,15 @@
 package ru.otus.homework
 
 fun main() {
-    println(joinStrings("Hello, ", "World!"))
+    val a: Int = 1             // Сразу указываем тип
+    val b = 2                  // Тип `Int` угадывается из контекста
+    val c: Int                 // Тип `Int` необходимо указать при отложенном присваивании
+    c = 3                      // Отложенное присваивание
+    val d = sum(a, b)          // Вызов функции и присваивание результата
+    val (e, f) = arrayOf(5, 6) // Деструктуризация массива
 }
+
+fun sum(a: Int, b: Int): Int = a + b
 
 fun joinStrings(a: String, b: String): String {
     return a + b
