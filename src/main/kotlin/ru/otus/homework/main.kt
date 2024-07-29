cf1e422bea8ef9b6064b7c6b0aff45ea6bd88ed1
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-07-29 11:23:42 +0300

    label break

--- src/main/kotlin/ru/otus/homework/main.kt
+++ src/main/kotlin/ru/otus/homework/main.kt
@@ -4,18 +4,21 @@ import kotlin.random.Random
 
 fun main() {
     var x = 10
-    while (x > 0) { // Сначала проверяем условие, потом выполняем тело цикла
+    var y: Int = -1
+
+    loop1@ while (x > 0) { // Метка loop1
         x--
+        y = 10
+        while (y > 0) {
+            y--
+            if (Random.nextBoolean()) {
+                break@loop1 // Прерывание по метке loop1
+            }
+        }
     }
-
-    do { // Сначала выполняется тело цикла, потом проверяется условие
-        val y = retrieveData()
-    } while (y != null) // y не видна за пределами цикла
+    println("x = $x, y = $y") // x = 9, y = 8
 }
 
-fun retrieveData(): String? {
-    return if (Random.nextBoolean()) "data" else null
-}
 
 fun joinStrings(a: String, b: String): String {
     return a + b
