e86fdcf55f6282b2446747cb9c8b62500cc15d60
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-07-29 10:34:10 +0300

    Array constructor

--- src/main/kotlin/ru/otus/homework/main.kt
+++ src/main/kotlin/ru/otus/homework/main.kt
@@ -1,11 +1,8 @@
 package ru.otus.homework
 
 fun main() {
-    val a1 = arrayOf(1, 2, 3, 4, 5)   // Создание массива из 5 произвольных элементов
-    println("Array size: " + a1.size) // Array size: 5
-    println("Element 0: " + a1[0])    // Element 0: 1
-    a1[0] = 100                       // Изменение элемента массива
-    println("Element 0: " + a1[0])    // Element 0: 100
+    val a1 = Array(5) { i: Int -> i + 1 }
+    println("Array contents: ${a1.joinToString()}")
 }
 
 fun joinStrings(a: String, b: String): String {
