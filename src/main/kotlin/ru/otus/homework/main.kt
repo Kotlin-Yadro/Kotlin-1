792411a363924dd16f11b469fcafd1d720b07a25
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-07-29 10:35:29 +0300

    Array concatenation

--- src/main/kotlin/ru/otus/homework/main.kt
+++ src/main/kotlin/ru/otus/homework/main.kt
@@ -1,8 +1,11 @@
 package ru.otus.homework
 
 fun main() {
-    val a1 = Array(5) { i: Int -> i + 1 }
-    println("Array contents: ${a1.joinToString()}")
+    val a1 = arrayOf(1, 2, 3)
+    val a2 = arrayOf(4, 5)
+    val a3 = a1 + a2 // a3 - новый массив!
+    val a4 = a3 + 6  // a4 - новый массив!
+    println("Array contents: ${a4.joinToString()}")
 }
 
 fun joinStrings(a: String, b: String): String {
